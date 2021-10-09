3050a26d85dd2bd90fa00a63535875941fee8a92
yuanzii <yyuan7142@gmail.com>
2021-10-09 14:11:09 +0630

    sss

--- backend/server/main.py
+++ backend/server/main.py
@@ -14,5 +14,5 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 app.register_blueprint(home)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=2040)
+    app.run(debug=True,host='0.0.0.0', port=2050)
 
--- frontend/client/src/views/home.vue
+++ frontend/client/src/views/home.vue
@@ -82,7 +82,7 @@ export default {
         this.load_data();
       } else {
         var send_info = value;
-        const path = this.$api + "/purchase_s_price_api";
+        const path = this.$api + "/cost_ratio_api";
         axios
           .post(path, send_info)
           .then((res) => {
