d4c8f660fbf09df562dd0853ec92e731cd5587e5
yuanzii <yyuan7142@gmail.com>
2021-10-09 12:03:51 +0630

    start1.1

--- backend/server/db/psql_psycopg2.py
+++ backend/server/db/psql_psycopg2.py
@@ -2,7 +2,7 @@ import psycopg2
 
 con = psycopg2.connect(
         host="127.0.0.1",
-        database="price_4",
+        database="price_5",
         user="postgres",
         password="yuan7142",
         port=5432
--- backend/server/db/psql_sqlalchemy.py
+++ backend/server/db/psql_sqlalchemy.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 
 engine_price = create_engine('postgresql+psycopg2:'
                              '//postgres:yuan7142'
-                             '@localhost:5432/price_4',
+                             '@localhost:5432/price_5',
                              echo=False
                              )
